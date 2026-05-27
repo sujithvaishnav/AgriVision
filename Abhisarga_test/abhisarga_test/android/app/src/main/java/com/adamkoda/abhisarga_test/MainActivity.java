8170bf16daa835b8c7feb2151b27a16fd484c3a2
AdamKoda2306 <adam.koda2306@gmail.com>
2026-05-27 17:24:29 +0530

    previous version of app

--- Abhisarga_test/abhisarga_test/android/app/src/main/java/com/adamkoda/abhisarga_test/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.adamkoda.abhisarga_test;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
---
+++ Abhisarga_test/android/app/src/main/java/com/adamkoda/abhisarga_test/MainActivity.java
@@ -0,0 +1,6 @@
+package com.adamkoda.abhisarga_test;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
